e4e54c2161590e6f980b574796e86c71b636148d
NotGyashu <110993626+NotGyashu@users.noreply.github.com>
2024-01-14 18:57:51 +0530

    done

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,18 +1,25 @@
 // get the library
 import mongoose from "mongoose";
 
-// connect the database
-mongoose.connect('mongodb://127.0.0.1:27017/TO-DO');
+// MongoDB Atlas connection string
+const atlasConnection =
+  "mongodb+srv://GyashuRahman:yt26RB6r3DrAzvD4@cluster0.tzsrmro.mongodb.net/?retryWrites=true&w=majority";
 
-//acquire the connection (to check if it is succesfull)
+// connect the database
+mongoose.connect(atlasConnection, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// acquire the connection (to check if it is successful)
 const db = mongoose.connection;
 
-// error 
-db.on('error', console.error.bind(console, 'error connecting to db'));
+// error
+db.on("error", console.error.bind(console, "error connecting to db"));
 
 // up and running then print the message
-
-db.once('open', function(){
-    console.log("successfully connect to the database");
+db.once("open", function () {
+  console.log("successfully connect to the database");
 });
+
 export default db;
